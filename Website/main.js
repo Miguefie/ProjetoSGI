53fd80b4980996be42d1164dbc2019d24ec65d35
Miguel <Fialho>
2019-12-17 13:48:47 +0000

    Mockup e Responsive do Site Quase Terminado

--- Website/main.js
+++ Website/main.js
@@ -1,32 +1,42 @@
-
-
-var container = document.getElementById('canvas');
-
 var geometria = new THREE.BoxGeometry( 1, 1, 1 )
 var material = new THREE.MeshNormalMaterial()
 var cubo = new THREE.Mesh( geometria, material )
 var cena = new THREE.Scene()
 cena.add( cubo )
-var renderer = new THREE.WebGLRenderer({canvas : canvas})
+
+var renderer = new THREE.WebGLRenderer();
+var camera = new THREE.PerspectiveCamera( 1, 1, 0.1, 1000)
+
+container = document.getElementById('container');
+renderer.setSize($(container).width(), $(container).height());
 container.appendChild(renderer.domElement);
-var camara = new THREE.PerspectiveCamera( 70, 800 / 600, 0.01, 1000)
-camara.position.z = 5
 
-
-camera.aspect = container.clientWidth / container.clientHeight;
-camera.updateProjectionMatrix();
-renderer.setSize(container.clientWidth, container.clientHeight);
-
-renderer.render( cena, camara )
+renderer.render( cena, camera )
 
 animar();
 
+var needsResize = false;
+function updateViewport() {
+    renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+    camera.fov = window.innerHeight / window.screen.height;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+updateViewport();
+
+
 function animar() 
 {
+    if ( needsResize ) {
+        needsResize = false;
+        updateViewport();
+    }
     requestAnimationFrame( animar )
     // mostrar...
-    renderer.render( cena, camara )
+    renderer.render( cena, camera )
     // atualizar posição do cubo
     cubo.rotateX(0.01)
     cubo.rotateY(0.02)
 }
+
+window.addEventListener('resize', function(){needsResize = true}); 

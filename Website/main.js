5bf8c9b5cf53cd245e14c0ac1abfa771f9494c23
Miguefie <miguefie2010@gmail.com>
2020-01-14 17:16:40 +0000

    Resonsive e Grid do Bootstrap Feito

--- Website/main.js
+++ Website/main.js
@@ -1,42 +1,84 @@
-var geometria = new THREE.BoxGeometry( 1, 1, 1 )
-var material = new THREE.MeshNormalMaterial()
-var cubo = new THREE.Mesh( geometria, material )
-var cena = new THREE.Scene()
-cena.add( cubo )
-
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera( 1, 1, 0.1, 1000)
-
-container = document.getElementById('container');
-renderer.setSize($(container).width(), $(container).height());
-container.appendChild(renderer.domElement);
-
-renderer.render( cena, camera )
-
-animar();
-
-var needsResize = false;
-function updateViewport() {
-    renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
-    camera.fov = window.innerHeight / window.screen.height;
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-}
-updateViewport();
 
 
-function animar() 
+function main() 
 {
-    if ( needsResize ) {
-        needsResize = false;
-        updateViewport();
+    const canvas = document.querySelector('#container');
+    const renderer = new THREE.WebGLRenderer({canvas});
+    const camera = new THREE.PerspectiveCamera( 75, 2, 0.1, 5);
+    camera.position.z = 2;
+
+
+    const scene = new THREE.Scene();
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
     }
-    requestAnimationFrame( animar )
-    // mostrar...
-    renderer.render( cena, camera )
-    // atualizar posição do cubo
-    cubo.rotateX(0.01)
-    cubo.rotateY(0.02)
+
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+
+    //Adicionar Objetos à Cena
+    function makeInstance(geometry, color, x) 
+    {
+        const material = new THREE.MeshPhongMaterial({color});
+    
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+    
+        cube.position.x = x;
+    
+        return cube;  
+    }
+
+    const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+    ];
+
+    function resizeRendererToDisplaySize(renderer) 
+    {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render(time)
+    {
+        time *= 0.001;
+
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          camera.updateProjectionMatrix();
+        }
+    
+        cubes.forEach((cube, ndx) => {
+          const speed = 1 + ndx * .1;
+          const rot = time * speed;
+          cube.rotation.x = rot;
+          cube.rotation.y = rot;
+        });
+    
+        renderer.render(scene, camera);
+    
+        requestAnimationFrame(render);
+   }
+    
+    requestAnimationFrame(render);
+    
 }
 
-window.addEventListener('resize', function(){needsResize = true}); 
+main();
